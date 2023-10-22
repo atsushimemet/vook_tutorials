4f9060452a41b9e4571da9ea1411f4b47724d701
atsushimemet <noap3b69n@gmail.com>
2023-10-22 14:32:35 +0900

    style: main.py

--- python/pydantic/main.py
+++ python/pydantic/main.py
@@ -1,14 +1,12 @@
 from models.user import User
 
+
 def main():
-    user_data = {
-        "name": "John Doe",
-        "email": "john.doe@example.com",
-        "age": 25
-    }
-    
+    user_data = {"name": "John Doe", "email": "john.doe@example.com", "age": 25}
+
     user = User(**user_data)
     print(user.dict())
+
 
 if __name__ == "__main__":
     main()
