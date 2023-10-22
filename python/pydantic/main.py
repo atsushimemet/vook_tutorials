4af218e1c54c8d7f43deda048be53328130e8911
atsushimemet <noap3b69n@gmail.com>
2023-10-22 14:46:05 +0900

    add: to send validation error

--- python/pydantic/main.py
+++ python/pydantic/main.py
@@ -1,11 +1,15 @@
 from models.user import User
+from pydantic import ValidationError
 
 
 def main():
     user_data = {"name": "John Doe", "email": "john.doe@example.com", "age": 25}
 
-    user = User(**user_data)
-    print(user.dict())
+    try:
+        user = User(**user_data)
+        print(user.dict())
+    except ValidationError as e:
+        print(e.errors())
 
 
 if __name__ == "__main__":
