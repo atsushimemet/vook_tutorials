e51bda76ee3271660abf25b56fafc28140a35016
atsushimemet <noap3b69n@gmail.com>
2023-10-22 14:46:19 +0900

    style: to del unnecessary code

--- python/pydantic/models/user.py
+++ python/pydantic/models/user.py
@@ -5,8 +5,3 @@ class User(BaseModel):
     name: constr(min_length=2, max_length=100)
     email: EmailStr
     age: int
-
-    class Config:
-        schema_extra = {
-            "example": {"name": "John Doe", "email": "john.doe@example.com", "age": 25}
-        }
