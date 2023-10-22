748a6b84e8896539cd1e0684c51fa0c00681d2e0
atsushimemet <noap3b69n@gmail.com>
2023-10-22 14:48:43 +0900

    add: to check mailstr

--- python/pydantic/main.py
+++ python/pydantic/main.py
@@ -5,7 +5,8 @@ from pydantic import ValidationError
 def main():
     # user_data = {"name": "John Doe", "email": "john.doe@example.com", "age": 25}
     # user_data = {"name": "John Doe", "email": "john.doe@example.com", "age": "25"}
-    user_data = {"name": "John Doe", "email": "john.doe@example.com", "age": "25a"}
+    # user_data = {"name": "John Doe", "email": "john.doe@example.com", "age": "25a"}
+    user_data = {"name": "John Doe", "email": "john.doe-example.com", "age": 25}
 
     try:
         user = User(**user_data)
