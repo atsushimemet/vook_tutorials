00162f2f6123232786fee010e1d3891fb60af8e8
atsushimemet <noap3b69n@gmail.com>
2023-10-22 14:47:47 +0900

    add: to check not int

--- python/pydantic/main.py
+++ python/pydantic/main.py
@@ -4,7 +4,8 @@ from pydantic import ValidationError
 
 def main():
     # user_data = {"name": "John Doe", "email": "john.doe@example.com", "age": 25}
-    user_data = {"name": "John Doe", "email": "john.doe@example.com", "age": "25"}
+    # user_data = {"name": "John Doe", "email": "john.doe@example.com", "age": "25"}
+    user_data = {"name": "John Doe", "email": "john.doe@example.com", "age": "25a"}
 
     try:
         user = User(**user_data)
