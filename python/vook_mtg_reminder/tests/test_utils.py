ab02cb4c6b7c22e3ae3428ed8e5ad7ad29282831
atsushimemet <noap3b69n@gmail.com>
2023-11-25 08:47:17 +0900

    add: tests

---
+++ python/vook_mtg_reminder/tests/test_utils.py
@@ -0,0 +1,25 @@
+# import pytest
+from datetime import datetime
+from utils import calculate_saturdays
+
+
+def test_calculate_saturdays():
+    # calculate_saturdays関数のテスト
+    year = 2023
+    month = 1  # 例えば2023年1月
+    first_saturday, third_saturday = calculate_saturdays(year, month)
+    assert first_saturday == datetime(2023, 1, 7)  # 2023年1月の第一土曜日
+    assert third_saturday == datetime(2023, 1, 21)  # 2023年1月の第三土曜日
+
+
+# @patch("requests.post")
+# def test_set_slack_reminder(mock_post):
+#     # set_slack_reminder関数のテスト
+#     date = "2023-01-07T09:00:00Z"  # ISOフォーマットの日付
+#     message = "テストリマインダー"
+#     mock_post.return_value.json.return_value = {"ok": True, "reminder": {"id": "Rm123"}}
+
+#     response = set_slack_reminder(date, message)
+#     mock_post.assert_called_once()
+#     assert response["ok"]
+#     assert "reminder" in response
